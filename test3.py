656ed12415374438e980bfb659a9adc733e6f64c
YakovBoiev <iakovboiev@gmail.com>
2020-11-24 21:39:48 +0100

    lesson1

---
+++ test3.py
@@ -0,0 +1,5 @@
+n = input("Введите число ")
+nn = int(n + n)
+nnn = int(n + n + n)
+n = int(n)
+print(n + nn + nnn)
---
+++ test6.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+dayCount = 1
+while a < b:
+    a = a * 1.1
+    dayCount += 1
+print(dayCount)
